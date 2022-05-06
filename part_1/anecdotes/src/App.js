6f83f544ea89406d874c9b6002567f1f2c8ec961
Mara Braslava <m.braslava@gmail.com>
2022-05-06 22:04:30 +0300

    anecdotes 1.14

--- part_1/anecdotes/src/App.js
+++ part_1/anecdotes/src/App.js
@@ -1,11 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import anecdotes from "./anecdotes";
 
 function App() {
     const pointsArray = new Uint8Array(anecdotes.length);
     const [selected, setSelected] = useState(0);
     const [points, setPoints] = useState(pointsArray);
+    const [mostVoted, setMostVoted] = useState({ text: "", points: 0 });
+
     const getRandomIndex = (max) => Math.floor(Math.random() * max);
+    const findBiggestNumberIndex = (arr) => {
+        let max = arr[0];
+        let maxIndex = 0;
+        for (var i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                maxIndex = i;
+                max = arr[i];
+            }
+        }
+        console.log("maxindex", maxIndex);
+        return maxIndex;
+    };
 
     const handleChangeAnecdote = () => {
         let index = getRandomIndex(anecdotes.length);
@@ -24,14 +38,31 @@ function App() {
         console.log(points);
     };
 
+    useEffect(() => {
+        let maxVoteIndex = findBiggestNumberIndex(points);
+        setMostVoted({
+            text: anecdotes[maxVoteIndex],
+            points: points[maxVoteIndex],
+        });
+        console.log(mostVoted);
+    }, [points]);
+
     return (
         <div className="App">
             <div className="content">
+                <h1>Anecdote of the day</h1>
                 <p>{anecdotes[selected]}</p>
                 <p>This anecdote has {points[selected]} votes</p>
+                <button onClick={handleChangeAnecdote}>Next anecdote</button>
+                <button onClick={handleVote}>Vote</button>
             </div>
-            <button onClick={handleChangeAnecdote}>Next anecdote</button>
-            <button onClick={handleVote}>Vote</button>
+            {mostVoted && (
+                <div className="results">
+                    <h1>Anecdote with most votes</h1>
+                    <p>{mostVoted.text}</p>
+                    <p>This anecdote has {mostVoted.points} votes</p>
+                </div>
+            )}
         </div>
     );
 }
