a2d3682a662a5dda45b92cbb7fb465b49f04680e
Mara Braslava <m.braslava@gmail.com>
2022-05-08 19:12:41 +0300

    phonebook 2.11

--- part_2/phonebook/src/App.js
+++ part_2/phonebook/src/App.js
@@ -1,18 +1,26 @@
-import { useState } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import Person from "./components/Person";
 import Search from "./components/Search";
 import AddEntryForm from "./components/AddEntryFrom";
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: "Arto Hellas", number: "040-123456", id: 1 },
-        { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-        { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-        { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-    ]);
+    const [persons, setPersons] = useState([]);
     const [newName, setNewName] = useState("");
     const [newPhone, setNewPhone] = useState("");
     const [searchTerm, setSearchTerm] = useState("");
+
+    const getPersonsData = async () => {
+        try {
+            const response = await axios.get("http://localhost:3001/db");
+            setPersons(response.data.persons);
+            console.log(response.data)
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {getPersonsData()}, []);
 
     const handleNameChange = (e) => setNewName(e.target.value);
 
@@ -25,7 +33,7 @@ const App = () => {
                 (person) => person.name.toLowerCase() === newName.toLowerCase()
             ).length > 0
         ) {
-            alert(`${newName} is laready in the phonebook!`);
+            alert(`${newName} is already in the phonebook!`);
             return;
         }
         const personObject = {
